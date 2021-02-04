0be8145ee290c296542d33a8369e2243bee66fd1
AnaH-1907 <anaisgagneur@gmail.com>
2021-02-04 16:59:15 +0100

    ajout de style et refacto pour mieux se retrouver

--- src/pages/Studies/Exam/FormExam/index.tsx
+++ src/pages/Studies/Exam/FormExam/index.tsx
@@ -3,26 +3,8 @@ import {useForm, Controller} from 'react-hook-form';
 import {NavLink} from 'react-router-dom';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
 // import { useHistory } from "react-router-dom";
 
-
-const schema = yup.object().shape({
-    title: yup.string().required(),
-    cover: yup.string().required(),
-    question: yup.string().required(),
-    answer0: yup.string().required(),
-    answer1: yup.string().required(),
-    answer2: yup.string(),
-    answer3: yup.string(),
-    isTrue: yup.string().required(),
-    funFact: yup.string().required(),
-    isPublished: yup.bool().required(),
-    publishedOn: yup.date().default(function () {
-        return new Date();
-      }),
-  });
 
 
 const FormExam = () => {
@@ -53,9 +35,7 @@ const FormExam = () => {
  
 
     // let history = useHistory();
-    const {register, handleSubmit, formState, control, errors} = useForm(
-        { resolver: yupResolver(schema)}
-      );
+    const {register, handleSubmit, formState, control, errors} = useForm();
     const {isSubmitting, isSubmitSuccessful} = formState;
 
     const onSubmit = data => {
@@ -63,13 +43,14 @@ const FormExam = () => {
         return true;
     }
 
+    console.log(isSubmitSuccessful);
+
     // if(isSubmitSuccessful){
     //     history.push("/studies/exam");
     // }
 
-    return <form onSubmit={handleSubmit(onSubmit)}>
+    return <div className="container mx-auto px-8 max-w-2xl grid gap-10 pt-32 pb-12">
 
-            <h2>Ajouter un Quiz</h2>
                 {
                     isSubmitSuccessful && 
                     <div className="text-white px-6 py-4 border-0 rounded relative mb-4 bg-green-500">
@@ -77,7 +58,7 @@ const FormExam = () => {
                             <i className="fas fa-bell" />
                         </span>
                         <span className="inline-block align-middle mr-8">
-                            Le quiz a bien été enregistré
+                            L'exam a bien été enregistré
                         </span>
                         <button className="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none">
                         <NavLink to="/studies/exam" className="">
@@ -87,7 +68,10 @@ const FormExam = () => {
                     </div>
                 }
 
-            <div className="container mx-auto px-8 max-w-2xl grid gap-10 pt-32 pb-12">
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+            <h2>Ajouter un exam</h2>
+
                 <label className="flex items-center justify-between cursor-pointer">
                     <p>Titre</p><input type="text" className="mb-12 input" placeholder="Titre" name="title" ref={register}/>
                 </label>
@@ -109,7 +93,7 @@ const FormExam = () => {
                 return (
                     <>
                         <label className="flex items-center justify-between cursor-pointer">
-                            <p>Réponse 1</p><input type="text" className="mb-12 input" placeholder="Réponse" name="answer" value={x.answer} ref={register}
+                            <p>Réponse</p><input type="text" className="mb-12 input" placeholder="Réponse" name="answer" value={x.answer} ref={register}
                             onChange={e => handleAnswerChange(e, i)}
                             />
                         </label>
@@ -123,15 +107,14 @@ const FormExam = () => {
                         {errors.isTrue && <span>{errors.isTrue.message}</span>}
 
                         <div>
-                            {answerList.length !== 1 && <button
+                            {answerList.length !== 1 && <button 
+                            className="bg-purple-500 text-white active:bg-purple-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1"
                             onClick={() => handleRemoveClick(i)}>Supprimer</button>}
                         </div>
                     
                     </>
                     );
                 })}
-
-                {answerList.length + 0 < maxAnswer && <button onClick={handleAddClick}>Ajouter</button>}
 
                 <label className="flex items-center justify-between cursor-pointer">
                     <p>Anecdote</p><input type="text" className="mb-12 input" placeholder="Anecdote" name="funFact" ref={register}/>
@@ -148,6 +131,7 @@ const FormExam = () => {
                     <Controller
                         control={control}
                         name="publishedOn"
+                        defaultValue={new Date()}
                         render={({ onChange, onBlur, value }) => (
                             <DatePicker 
                             selected={value}
@@ -159,10 +143,12 @@ const FormExam = () => {
                 </label>
                 {errors.publishedOn && <span>{errors.publishedOn.message}</span>}
 
-                <button className="btn btn-primary" disabled={isSubmitting}>Valider</button>
-            </div>
+                <button className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" disabled={isSubmitting}>Valider</button>
     </form>
-        
+
+    {answerList.length + 0 < maxAnswer && <button className="bg-indigo-500 text-white active:bg-indigo-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" onClick={handleAddClick}>Ajouter une réponse</button>}
+    
+    </div>
 }
 
 export default FormExam;
